5cb53fcac2a82abff0f610a3038fe31149922c41
Larissa Andrade <44838761+lariandrade@users.noreply.github.com>
2023-05-09 14:48:50 -0300

    cadastro paciente

---
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,7 @@
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.medico.Especialidade;
+
+public record DadosCadastroPaciente(String nome, String email, String cpf, String telefone, DadosEndereco endereco) {
+}
