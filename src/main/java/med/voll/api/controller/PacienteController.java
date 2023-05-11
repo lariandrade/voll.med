0f573f5dd34921e722aab36e18e5a94f4ffd3339
Larissa Andrade <44838761+lariandrade@users.noreply.github.com>
2023-05-11 17:54:46 -0300

    cadastrando paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,7 +1,11 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pacientes")
 public class PacienteController {
 
+    @Autowired
+    PacienteRepository repository;
+
     @PostMapping
-    public void cadastrarPaciente(@RequestBody DadosCadastroPaciente dados){
-        System.out.println(dados);
+    public void cadastrarPaciente(@RequestBody @Valid DadosCadastroPaciente dados){
+
+        repository.save(new Paciente(dados));
     }
 
 }
--- src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -1,7 +1,23 @@
 package med.voll.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 import med.voll.api.medico.Especialidade;
 
-public record DadosCadastroPaciente(String nome, String email, String cpf, String telefone, DadosEndereco endereco) {
+public record DadosCadastroPaciente(
+
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}") String cpf,
+        String telefone,
+
+        DadosEndereco endereco) {
 }
